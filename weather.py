2e076f28540833508cfa121ea710cf3d472ce0fa
Kristianuruplarsen <kristianuruplarsen@gmail.com>
2017-07-18 20:40:45 +0200

    prepare for website construction

--- weather.py
+++ weather.py
@@ -16,7 +16,7 @@ def get_loc_by_ip():
     return [j['latitude'], j['longitude'], j['city'], j['region_name'], j['country_name']]
 
 
-#def get_loc_by_gps():
+# def get_loc_by_gps():
 #
 #
 #
@@ -37,39 +37,64 @@ def get_weather_at_loc(pk):
     return resp
 
 
-#def illustrate_currently(weather):
+
+
+### PYGAL VISUALIZATIONS
+#   show_windspeed(weather): plots average and max windspeed
+#       weather: output from get_weather_at_loc
+#
+#   show_temp_overday(weather): plots average and max teperature per hour
+#       weather: output from get_weather_at_loc
+#
+#   show_winddir_overday(weather): plots winddirection per hour
+#       weather: output from get_weather_at_loc
+#
+#   show_rain_overdays(weather): plots daily probability of rain and cloud coverage for the comming 8 days
+#       weather: output from get_weather_at_loc
+#
+#   show_temperature(weather): plots current temperature
+#       weater: output from get_weather_at_loc
+
+from pygal.style import Style
 
 
 weather = get_weather_at_loc(key)
 
-weather
-
-### PYGAL VIZ
-#
-# show some figures about the weather comming up
-#
-#
 
 
+custom_style = Style(
+  background='transparent',
+  plot_background='transparent',
+  foreground='#53E89B',
+  foreground_strong='#53A0E8',
+  foreground_subtle='#630C0D',
+  opacity='.6',
+  opacity_hover='.9',
+  transition='400ms ease-in',
+  colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'))
 
-def show_windspeed(weather):
+
+
+
+def show_windspeed(weather, name):
     gauge = pygal.SolidGauge(
         half_pie=True, inner_radius= 0.7,
-        style=pygal.style.styles['default'](value_font_size=10))
+        style = custom_style)
+        #style=pygal.style.styles['default'](value_font_size=10))
 
     formatter = lambda x: '{:.10g}m/s'.format(x)
-    gauge.value_formatter = percent_formatter
+    gauge.value_formatter = formatter
 
     gauge.add('Gusts', [{'value': weather['currently']['windSpeed'], 'max_value': weather['currently']['windGust']}],
               formatter=formatter)
     gauge.add('Windspeed', [{'value': weather['currently']['windSpeed'], 'max_value':15}],
               formatter=formatter)
-    return gauge.render()
+    return gauge.render_to_file('./svg/' + name + '.svg')
 
-show_windspeed(weather)
+show_windspeed(weather, 'windspeed')
 
 
-def show_temp_overday(weather):
+def show_temp_overday(weather, name):
     d = {'time': [], 'temp': []}
     for i in range(0,len(weather['hourly']['data'])):
         time = datetime.datetime.fromtimestamp(weather['hourly']['data'][i]['time']).strftime('%H')
@@ -77,16 +102,34 @@ def show_temp_overday(weather):
         d['temp'].append(weather['hourly']['data'][i]['temperature'])
 
 
-    line_chart = pygal.Line()
+    line_chart = pygal.Line(style = custom_style)
     line_chart.title = 'Hourly temperatures - next 48 hrs'
     line_chart.x_labels = map(str, d['time'])
     line_chart.add('temperature', d['temp'])
-    return line_chart.render()
+    return line_chart.render_to_file('./svg/' + name + '.svg')
 
-show_temp_overday(weather)
+show_temp_overday(weather, 'temp_overday')
 
 
-def show_winddir_overday(weather):
+def show_temp_overdays(weather, name):
+    d = {'time': [], 'temp_max': [], 'temp_min': []}
+    for i in range(0,len(weather['daily']['data'])):
+        time = datetime.datetime.fromtimestamp(weather['daily']['data'][i]['time']).strftime('%d')
+        d['time'].append(time)
+        d['temp_max'].append(weather['daily']['data'][i]['temperatureMax'])
+        d['temp_min'].append(weather['daily']['data'][i]['temperatureMin'])
+
+    line_chart = pygal.Bar(style=custom_style)
+    line_chart.title = 'Daily temperatures - 8 days'
+    line_chart.x_labels = map(str, d['time'])
+    line_chart.add('daily max', d['temp_max'])
+    line_chart.add('daily min', d['temp_min'])
+    return line_chart.render_to_file('./svg/' + name + '.svg')
+
+show_temp_overdays(weather, 'temp_overdays')
+
+
+def show_winddir_overday(weather, name1, name2):
     d = {'time': [], 'winddir': []}
     for i in range(0,len(weather['hourly']['data'])):
         time = datetime.datetime.fromtimestamp(weather['hourly']['data'][i]['time']).strftime('%H')
@@ -94,7 +137,7 @@ def show_winddir_overday(weather):
         d['winddir'].append(weather['hourly']['data'][i]['windBearing'])
 
 
-    line_chart = pygal.Line()
+    line_chart = pygal.Line(style=custom_style)
     line_chart.title = 'Daily wind bearing'
     line_chart.x_labels = map(str, d['time'])
     line_chart.add('Wind direction', d['winddir'])
@@ -105,45 +148,35 @@ def show_winddir_overday(weather):
     radar_chart.add('Wind direction', d['winddir'])
     radar_chart.render()
 
-    return [radar_chart.render(),line_chart.render()]
+    return [radar_chart.render_to_file('./svg/' + name1 + '.svg'),line_chart.render_to_file('./svg/' + name2 + '.svg')]
 
-show_winddir_overday(weather)[0]
-show_winddir_overday(weather)[1]
-
+show_winddir_overday(weather, 'windbearing_line','windbearing_radar')
 
 
-line_chart = pygal.Bar()
-line_chart.title = 'Browser usage evolution (in %)'
-line_chart.x_labels = map(str, range(2002, 2013))
-line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-line_chart.render()
-
-def show_rain_overdays(weather):
+def show_rain_overdays(weather, name):
     d = {'time': [], 'rain': [], 'clouds': []}
     for i in range(0,len(weather['daily']['data'])):
         time = datetime.datetime.fromtimestamp(weather['daily']['data'][i]['time']).strftime('%d')
         d['time'].append(time)
         d['rain'].append(weather['daily']['data'][i]['precipProbability'])
         d['clouds'].append(weather['daily']['data'][i]['cloudCover'])
-    line_chart = pygal.Bar()
+
+    line_chart = pygal.Bar(style = custom_style)
     line_chart.title = 'Daily rain probability'
     line_chart.x_labels = map(str, d['time'])
     line_chart.add('Risk of rain', d['rain'])
     line_chart.add('Cloud cover', d['clouds'])
-    return line_chart.render()
+    return line_chart.render_to_file('./svg/' + name + '.svg')
 
-show_rain_overdays(weather)
+show_rain_overdays(weather, 'rain')
 
 
 
-def show_temperature(weather):
-    gauge_chart = pygal.Gauge(human_readable=True)
+def show_temperature(weather, name):
+    gauge_chart = pygal.Gauge(human_readable=True, style = custom_style)
     gauge_chart.title = 'Current temperature'
     gauge_chart.range = [0, 35]
     gauge_chart.add('temperature', weather['currently']['temperature'])
-    return gauge_chart.render()
+    return gauge_chart.render_to_file('./svg/' + name + '.svg')
 
-show_temperature(weather)
+show_temperature(weather, 'temp_now')
