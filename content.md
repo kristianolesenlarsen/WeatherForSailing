e991bd1bb5052d2f5d0517d42389313cb3d84c80
Kristianuruplarsen <kristianuruplarsen@gmail.com>
2017-07-30 14:24:57 +0200

    content

--- content.md
+++ content.md
@@ -21,6 +21,29 @@ layout: null
   </head>
 
   <body>
+
+
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="test.html">WeatherForSailing</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
     <div class="container">
   <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
@@ -48,7 +71,7 @@ layout: null
         <h3>Two columns</h3>  
         <div class="row">
           <div class="col-md-8">
-          <img class="img-responsive" src="map-eu.png">
+          <img class="img-responsive" src="map-eu2.png">
           </div>
           <div class="col-md-4"> Lorem Ipsum</div>
         </div>
--- csvifyGRIB.R
+++ csvifyGRIB.R
@@ -1,8 +1,6 @@
 # This is so far the only way i've been able to get GRIB data to a more usable format
 
 
-install.packages(c('rgdal', 'viridis'))
-
 library("rgdal")
 library("jsonlite")
 library("ggmap")
@@ -15,18 +13,23 @@ setwd("C:/Users/Kristian/Documents/GitHub/WeatherForSailing/data")
 
 # fix some easy way to change this
 fname = "25N,70N,300W,30W29-00.grb"
+#fname = '40N,60N,140W,120W29-00.grb'
 #fname = "gfsanl_4_20170726_0000_006.grb2"
 
 
 gribdata = readGDAL(fname)
 df = as.data.frame(gribdata)
+rm(gribddata)
 
 
+#df$speed = sqrt(df$band4^2 + df$band7^2)
 df$speed = sqrt(df$band1^2 + df$band2^2)
 df$unif = runif(length(df$x),0,1)
 
-map = get_map(c(5, 55) , zoom = 3, color = 'bw')
+#map = get_map(c(-130, 55) , zoom = 3, maptype = 'satellite')
+map = get_map(c(5, 55) , zoom = 3, maptype = 'satellite')
 
+#ggmap(map)
 
 setwd("C:/Users/Kristian/Documents/GitHub/WeatherForSailing")
 
@@ -34,14 +37,15 @@ setwd("C:/Users/Kristian/Documents/GitHub/WeatherForSailing")
 
 p = ggmap(map) +
 #  geom_point(data = df, aes(x = x, y = y))# +
-#  geom_segment(data = subset(df, unif < 0.1), aes(x = x, y = y, xend = x + band1*0.01, yend = y+band2)) +
-  geom_tile(data = df, aes(x = x, y = y, z = speed, fill = speed),  alpha = 0.3) +
-  geom_contour(data = df, aes(x = x, y = y, z = speed, colour = ..level..), alpha = 0.6) +
-  scale_color_viridis(option = "plasma") +
-  scale_fill_viridis(option = "plasma") +
+#  geom_segment(data = subset(df, unif < 0.1), aes(x = x, y = y, xend = x + band1*0.01, yend = y+band2), arrow = arrow(length = unit(0.5, 'cm'))) +
+  geom_tile(data = df, aes(x = x, y = y,  fill = speed, alpha = speed), alpha = 0.6) +
+  geom_contour(data = df, aes(x = x, y = y, z = speed, colour = ..level..), bins = 5) +
+  scale_fill_gradient2(low = 'blue', mid = 'green', high = 'yellow', midpoint = 8) +
+  scale_color_gradient2(low = 'blue', mid = 'green', high = 'yellow', midpoint = 8) +
+ # scale_color_viridis(option = "inferno") +
+  #scale_fill_viridis(option = "inferno") #+
   theme_nothing()
-
 
 p
 
-ggsave('map-eu.png', p)
+ggsave('map-eu2.png')
